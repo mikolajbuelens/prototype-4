33d9a63d1e3f1e85b62ec11c0db3027b3831bccb
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2024-12-02 11:40:54 +0100

    fix: resolve issue where tailwind didn't work outside of root page

--- login-system/src/app/dashboard/page.tsx
+++ login-system/src/app/dashboard/page.tsx
@@ -5,32 +5,42 @@ import { createClient, Provider } from "@supabase/supabase-js";
 import styles from "../app/ui/login.module.css";
 import { useState, useEffect } from "react";
 
-
 export default function Dashboard() {
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   );
 
- 
-
   const [user, setUser] = useState("");
 
-  useEffect(() => {
-    async function getUser() {
-      const { data: {user}, error,} = await supabase.auth.getUser();
-      // setUser(user.email);
-      console.log(user.email);
-      setUser(user.email);
-    }
-    getUser();
-  }, [supabase]);
+  // useEffect(() => {
+  //   async function getUser() {
+  //     const {
+  //       data: { user },
+  //       error,
+  //     } = await supabase.auth.getUser();
+  //     // setUser(user.email);
+  //     console.log(user.email);
+  //     setUser(user.email);
+  //   }
+  //   getUser();
+  // }, [supabase]);
 
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <header className="w-screen flex flex-col gap-4 items-center bg-red-500">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        ></Image>
+        <h1 className="bg-red-600">test</h1>
+      </header>
       <h1 className="text-4xl font-bold text-center">Dashboard</h1>
       <h2 className="text-2xl font-bold text-center">User is: {user}</h2>
-    
     </main>
   );
 }
