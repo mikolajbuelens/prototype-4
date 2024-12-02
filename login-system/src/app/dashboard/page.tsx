e12377e175e8d5d023ea4f45281e6339a01687b8
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2024-12-02 22:24:04 +0100

    feat: fetch repo data when logged in with github

--- login-system/src/app/dashboard/page.tsx
+++ login-system/src/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { createClient, Provider } from "@supabase/supabase-js";
 import styles from "../app/ui/login.module.css";
 import { useState, useEffect } from "react";
+import { use } from "react";
 
 export default function Dashboard() {
   const supabase = createClient(
@@ -12,22 +13,76 @@ export default function Dashboard() {
   );
 
   const [user, setUser] = useState("");
+  const [providerToken, setProviderToken] = useState("");
+  const [repos, setRepos] = useState([]);
 
-  // useEffect(() => {
-  //   async function getUser() {
-  //     const {
-  //       data: { user },
-  //       error,
-  //     } = await supabase.auth.getUser();
-  //     // setUser(user.email);
-  //     console.log(user.email);
-  //     setUser(user.email);
-  //   }
-  //   getUser();
-  // }, [supabase]);
+  useEffect(() => {
+    async function getUser() {
+      const {
+        data: { user },
+        error,
+      } = await supabase.auth.getUser();
+      // setUser(user.email);
+      console.log(user);
+      console.log(user.email);
+      setUser(user.email);
+    }
+    getUser();
+  }, [supabase]);
+
+
+  async function handleLogout() {
+    let { error } = await supabase.auth.signOut();
+    if (error) console.log("Error logging out:", error.message);
+    window.location.href = "/";
+  }
+
+
+
+async function getProviderToken(){
+  const session = await supabase.auth.getSession();
+  console.log(session.data.session.provider_token)
+  setProviderToken(session.data.session.provider_token)
+  
+  // console.log(session.provider_token)
+  // return session
+}
+
+
+
+useEffect(() => {
+  async function fetchRepos() {
+    if (providerToken) {
+      const response = await fetch('https://api.github.com/user/repos', {
+        headers: {
+          Authorization: `Bearer ${providerToken}`,
+        },
+      });
+      const data = await response.json();
+      setRepos(data);
+      console.log(data);
+    } else{
+      console.error('No provider token')
+    }
+  }
+  fetchRepos();
+}, [providerToken]);
+
+
+useEffect(() => {
+  getProviderToken()
+}
+, [supabase]);
+
+
+
+useEffect(() => {
+  console.log(repos)
+}
+, [repos]);
 
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <main className="flex flex-col row-start-2 items-center sm:items-start">
       <header className="w-screen flex flex-col gap-4 items-center border-b border-gray-500 p-4">
         <Image
           className="dark:invert"
@@ -45,7 +100,49 @@ export default function Dashboard() {
           DASHBOARD
         </div>
       </header>
-      <h2 className="text-2xl font-bold text-center">User is: {user}</h2>
+      <div className="h-screen flex flex-row gap-4 items-center">
+        <div className="sidebar  h-16 flex items-center justify-center">
+          <div className="flex flex-col gap-4 border-r border-gray-500 pr-5 pl-5 h-screen">
+            <a href="/dashboard" className="text-lg font-bold mt-5">
+              Dashboard
+            </a>
+            <a href="/app/profile" className="text-lg font-bold">
+              Profile
+            </a>
+            <button onClick={handleLogout} className="text-lg font-bold">
+              Logout
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 items-center w-screen h-screen ">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <div className="flex flex-col gap-4 items-center">
+            <h2 className="text-xl font-bold">Welcome to the dashboard</h2>
+            <h2 className="text-2xl font-bold text-center">User is: {user}</h2>
+          </div>
+
+          <table className="table-auto">
+            <thead>
+              <tr>
+                <th className="px-4 py-2">Name</th>
+                <th className="px-4 py-2">Language</th>
+                <th className="px-4 py-2">Url</th>
+              </tr>
+            </thead>
+            <tbody>
+              {repos.map((repo) => (
+                <tr key={repo.id}>
+                  <td className="border px-4 py-2">{repo.name}</td>
+                  <td className="border px-4 py-2">{repo.language}</td>
+                  <td className="border px-4 py-2">
+                    <a href={repo.html_url}>{repo.html_url}</a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </main>
   );
 }
