2439e1e29dd17ad5d5f7aaa579ab64f80d10868a
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2024-12-02 12:04:04 +0100

    WIP style: add header to dashboard

--- login-system/src/app/dashboard/page.tsx
+++ login-system/src/app/dashboard/page.tsx
@@ -28,18 +28,23 @@ export default function Dashboard() {
 
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <header className="w-screen flex flex-col gap-4 items-center bg-red-500">
+      <header className="w-screen flex flex-col gap-4 items-center border-b border-gray-500 p-4">
         <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={150}
+          height={25}
           priority
         ></Image>
-        <h1 className="bg-red-600">test</h1>
+        <div className="flex gap-2">
+          LOGIN SYSTEM
+          <div className="flex gap-2">
+            <div className="m-2 w-2 h-2 bg-white rounded-full"></div>
+          </div>
+          DASHBOARD
+        </div>
       </header>
-      <h1 className="text-4xl font-bold text-center">Dashboard</h1>
       <h2 className="text-2xl font-bold text-center">User is: {user}</h2>
     </main>
   );
